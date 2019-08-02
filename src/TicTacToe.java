c6aa20676e49fa5a2b21991ff48d790bd5feca97
league <league@imac-7.attlocal.net>
2019-08-01 20:00:58 -0700

    Game (IP)

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,4 +1,17 @@
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class TicTacToe {
+public static void main(String[] args) {
+	TicTacToe tictactoe = new TicTacToe();
+}
+JFrame frame = new JFrame();
+JPanel panel = new JPanel();
+
+
+void setup() {
+	frame.add(panel);
+	frame.setVisible(true);
+}
 
 }
